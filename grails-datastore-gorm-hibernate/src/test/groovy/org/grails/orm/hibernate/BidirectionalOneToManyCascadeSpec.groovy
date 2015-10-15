0b222776eeb94cbea5ed47e7388f3ec6443442f5
Joe McCall <joemccall@intrepid-networks.com>
2015-10-15 10:59:03 -0400

    Simplify test to narrow down problem
    Turns out this behavior is not specific to one-to-many.

--- grails-datastore-gorm-hibernate/src/test/groovy/org/grails/orm/hibernate/BidirectionalOneToManyCascadeSpec.groovy
+++ grails-datastore-gorm-hibernate/src/test/groovy/org/grails/orm/hibernate/BidirectionalOneToManyCascadeSpec.groovy
@@ -9,79 +9,43 @@ import spock.lang.Issue
 class BidirectionalOneToManyCascadeSpec extends GormSpec {
 
     @Issue('https://github.com/grails/grails-core/issues/9290')
-    void "Test that child is saved correctly when associating only the owning side"() {
+    void "Multiple levels are saved"() {
         when: "An owner is saved by the inverse child is not associated"
-        Team padres = new Team(
-                name: "Padres",
-                city: "San Diego"
+        def john = new TestPerson(
+                head: new TestHead(
+                        face: new TestFace()
+                )
         )
-
-
-        def contract = new Contract(
-                expiration: new Date(),
-                salary: 40_000_000
-        )
-
-        def player = new Player(
-                firstName: "John",
-                lastName: "Doe",
-                position: "Pitcher",
-                contract: contract
-        )
-
-        // workaround: this should not be needed!
-//        contract.player = player
-
-        padres.addToPlayers(player)
-        padres.save(flush: true)
+        john.save(flush: true)
         session.clear()
 
-        then: 'the team exists'
-        Team.count() == 1
-
-        and: 'the associations are created'
-        player.team == padres
-        contract.player == player
+        then: 'the person exists'
+        TestPerson.count() == 1
     }
 
     @Override
     List getDomainClasses() {
-        [Team, Player, Contract]
+        [
+                TestPerson,
+                TestHead,
+                TestFace
+        ]
     }
 
 }
 
 @Entity
-class Team {
-
-    String name
-    String city
-
-    static hasMany = [players: Player]
-
-    static constraints = {
-    }
+class TestPerson {
+    static hasOne = [head: TestHead]
 }
 
 @Entity
-class Player {
-
-    String firstName
-    String lastName
-    String position
-
-    static belongsTo = [team: Team]
-    static hasOne = [contract: Contract]
+class TestHead {
+    static belongsTo = [person: TestPerson]
+    static hasOne = [face: TestFace]
 }
 
 @Entity
-class Contract {
-
-    Date expiration
-    BigDecimal salary
-
-    static belongsTo = [player: Player]
-
-    static constraints = {
-    }
+class TestFace {
+    static belongsTo = [head: TestHead]
 }
